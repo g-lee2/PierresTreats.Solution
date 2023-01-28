6fd34aab615398eb4e0d13528fa08181cfb14d8d
Grace Lee <gracelee9901@gmail.com>
2023-01-28 00:26:01 -0800

    Update home controller to view flavors not logged in

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -2,37 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
     private readonly BakeryContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    public HomeController(BakeryContext db)
     {
-      _userManager = userManager;
       _db = db;
     }
     [HttpGet("/")]
-    public async Task<ActionResult> Index()
+    public ActionResult Index()
     {
       Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
       Dictionary<string,object[]> model = new Dictionary<string, object[]>();
       model.Add("treats", treats);
-      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-      if (currentUser != null)
-      {
-        Flavor[] flavors = _db.Flavors
-                        .Where(entry => entry.User.Id == currentUser.Id)
-                        .ToArray();
-        model.Add("flavors", flavors);
-      }
+      model.Add("flavors", flavors);
       return View(model);
     }
   }
